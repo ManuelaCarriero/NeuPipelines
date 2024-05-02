9560806fcc29b17ec087966799952c393f70dcca
ManuelaCarriero <manuela.carriero1@gmail.com>
2024-05-02 14:43:27 +0200

    Added white matter and csf binary mask estimates

--- EstimatePVE.m
+++ EstimatePVE.m
@@ -2,63 +2,84 @@
 %and without Partial Fourier
 
 %Insert the filename if you are in the same folder of the data.
-img_path_pve = '/storage/shared/SANDI_240418/NIFTI/t1_mp2rage_sag_p3_iso_fast/Segmented/t1_mp2rage_MRF01UNIMaskedwithINV2_pve_1.nii.gz';
-img_path_map = '/storage/shared/SANDI_240418/NIFTI/Rsoma_to_T1.nii.gz';
-title_plot = 'Rsoma map Without Partial Fourier';
 
+%Rsoma map to T1
+img_path_pve1 = '/storage/shared/SANDI_240418/NIFTI/t1_mp2rage_sag_p3_iso_fast/Segmented/t1_mp2rage_UNIMaskedwithINV2_pve_1.nii.gz';
+img_path_pve0 = '/storage/shared/SANDI_240418/NIFTI/t1_mp2rage_sag_p3_iso_fast/Segmented/t1_mp2rage_UNIMaskedwithINV2_pve_0.nii.gz';
+img_path_pve2 = '/storage/shared/SANDI_240418/NIFTI/t1_mp2rage_sag_p3_iso_fast/Segmented/t1_mp2rage_UNIMaskedwithINV2_pve_2.nii.gz';
+img_path_map = '/storage/shared/SANDI_240418/NIFTI/RsomawPF_to_T1.nii.gz';
+title_plot = 'Rsoma map With Partial Fourier';
+
+%T1 to diffusion weighted image
+img_path_pve1 = '/storage/shared/SANDI_240418/NIFTI/pve1_to_DWIwoPF.nii.gz';
+img_path_pve0 = '/storage/shared/SANDI_240418/NIFTI/pve0_to_DWIwoPF.nii.gz';
+img_path_pve2 = '/storage/shared/SANDI_240418/NIFTI/pve2_to_DWIwoPF.nii.gz';
+img_path_map = '/storage/shared/SANDI_240229/SANDI-Matlab-Toolbox-Latest-Release-main/dataset240418tr3000woPF/SANDI_MainFolder/derivatives/SANDI_analysis/sub-01/ses-01/SANDI_Output/SANDI-fit_Rsoma.nii.gz';
+title_plot = 'Rsoma map Without Partial Fourier';
 
 Vhdr_map = spm_vol(img_path_map);
 V_map = spm_read_vols(Vhdr_map);
 
-thr_pve = 0:0.1:1;
+thr_pve = 0.1:0.1:0.9;
 mean_par = [];
-
+Vhdr_pve1 = spm_vol(img_path_pve1);
+pve1 = spm_read_vols(Vhdr_pve1);
+Vhdr_pve0 = spm_vol(img_path_pve0);
+pve0 = spm_read_vols(Vhdr_pve0);
+Vhdr_pve2 = spm_vol(img_path_pve2);
+pve2 = spm_read_vols(Vhdr_pve2);
 for value = thr_pve
 
-    Vhdr_pve = spm_vol(img_path_pve);
-    V_pve = spm_read_vols(Vhdr_pve);
 
     %
 
-    size(V_pve)
 
     %figure,imagesc(V_pve(:,:,150))
 
     %make binary mask
-    V_pve(V_pve<value) = 0;
-    V_pve(V_pve>value) = 1;
+    GM=pve1>value;
+    WM=pve2>value;
+    CSF=pve0>value;
 
     %figure,imagesc(V_pve(:,:,150))
 
     %
 
-    V_map_masked = V_pve.*V_map;
-
+    V_GM = GM.*V_map;
+    V_WM = WM.*V_map;
+    V_CSF = CSF.*V_map;
     %figure,imagesc(V_map_masked(:,:,150));
 
     %calculate mean of parameter value in one region of brain cortex
     %in a second moment, choose a precise region (like the temporal one)
+    %
+    %if value == 0.1 || value == 0.5 || value == 0.9
+        %
+        %figure,imagesc(V_WM(1:176,1:256,150));
+        %
+        %
+        %
+        %
+    %end
 
-    if value == 0 || value == 0.5 || value == 1
-
-        figure,imagesc(V_map_masked(100:150,120:180,150));
-
-
-
-
-    end
-
-    V_mm_ROI = V_map_masked(100:150,120:180,150);
-    Vmean = mean(V_mm_ROI,"all");
-
-    mean_par(end+1) = Vmean;
+    V_GM(V_GM==0)=NaN;
+    V_WM(V_WM==0)=NaN;
+    V_CSF(V_CSF==0)=NaN;
+    Vmean(1) = nanmean(V_GM(:));%Compute the arithmetic mean along the specified axis, ignoring NaNs.
+    Vmean(2) = nanmean(V_WM(:));
+    Vmean(3) = nanmean(V_CSF(:));
+    mean_par(end+1,:) = Vmean;
 
 end
 
 figure, plot(thr_pve, mean_par);
+%hold on
+%plot(thr_pve, mean_par_wo);
 xlabel('Threashold value');
 ylabel('Mean value of Rsoma');
 title(title_plot);
+
+
 
 
 
