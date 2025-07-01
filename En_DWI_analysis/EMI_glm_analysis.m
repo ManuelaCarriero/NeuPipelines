9d0fcd3af446c700b7ff803caff5e4897720a5f4
ManuelaCarriero <manuela.carriero1@gmail.com>
2025-07-01 19:41:46 +0200

    Fixed bug for bootstrapping analysis and added parameters which do not refer to soma in comments

--- En_DWI_analysis/EMI_glm_analysis.m
+++ En_DWI_analysis/EMI_glm_analysis.m
@@ -3,31 +3,86 @@ mean_fsoma=mean_fsoma_high_n_voxels;
 mean_rsoma=mean_rsoma_high_n_voxels;
 mean_fsup=mean_fsup_high_n_voxels;
 
+% mean_De=mean_De_high_n_voxels;
+% mean_fneurite=mean_fneurite_high_n_voxels;
+% mean_fextra=mean_fextra_high_n_voxels;
+% mean_fc=mean_fc_high_n_voxels;
+
 energy_scored=zscore(mean_energy);
 fsoma_scored=zscore(mean_fsoma);
 rsoma_scored=zscore(mean_rsoma);
 fsup_scored=zscore(mean_fsup);
+
+% De_scored=zscore(mean_De);
+% fneurite_scored=zscore(mean_fneurite);
+% fextra_scored=zscore(mean_fextra);
+% fc_scored=zscore(mean_fc);
 
 energy_tr=energy_scored';
 fsoma_tr=fsoma_scored';
 rsoma_tr=rsoma_scored';
 fsup_tr=fsup_scored';
 
+% De_tr=De_scored';
+% fneurite_tr=fneurite_scored';
+% fextra_tr=fextra_scored';
+% fc_tr=fc_scored';
+%% GLM and PLS models (single run)
+%GLM
+
+%one with everything
+% y=energy_tr;
+% X=[fsoma_tr rsoma_tr fsup_tr De_tr fneurite_tr fextra_tr fc_tr];
+
+%one with only soma characteristics
+y=energy_tr;
+X=[fsoma_tr rsoma_tr fsup_tr ];
+
+fitglm(X,y,'linear')
+
+%PLS with cross validation
+s_x=size(X);
+ncomp=s_x(2);
+
+[XL,yl,XS,YS,beta,PCTVAR,MSE,stats] = plsregress(X,y,ncomp); %with n_comp=1 fsoma is positive as we expect from corr.
+
+%nested cross validation
+%k-fold cross validation to select the right number of components
+[XL,yl,XS,YS,beta,PCTVAR,PLSmsep] = plsregress(X,y,ncomp,'CV',10);
+
+min_err=min(PLSmsep(2,:));%min MSE for the predictor variable
+idx=find(PLSmsep(2,:)==min_err);
+new_ncomp=idx-1;
+
+[XL,yl,XS,YS,beta,PCTVAR,MSE,stats] = plsregress(X,y,new_ncomp);
+
+
+figure, plot(1:ncomp+1,PLSmsep(2,:))
 %% Bootstrapping to obtain a distribution of beta values and the error
 %This is to create the random data stream for reproducibility
-for i=1:10000
-    disp(i)
-    s=RandStream('mlfg6331_64','Seed',i);
-    energy_sampled = datasample(s, energy_tr, length(energy_tr),'Replace',true);
-    rsoma_sampled = datasample(s, rsoma_tr, length(rsoma_tr),'Replace',true);
-    fsoma_sampled = datasample(s, fsoma_tr, length(fsoma_tr),'Replace',true);
-    fsup_sampled = datasample(s, fsup_tr, length(fsup_tr),'Replace',true);
+%s=RandStream('mlfg6331_64','Seed',i);
+for i=1:1000
+    disp(i)    
+    %try to sample from 1 to the length and then use the index 
+    % for each parameter
 
+    idx_samples=datasample(1:length(energy_tr),length(energy_tr),'Replace',true);
 
+    energy_sampled = energy_tr(idx_samples);
+    rsoma_sampled = rsoma_tr(idx_samples);
+    fsoma_sampled = fsoma_tr(idx_samples);
+    fsup_sampled = fsup_tr(idx_samples);
+% 
+%     De_sampled = De_tr(idx_samples);
+%     fneurite_sampled = fneurite_tr(idx_samples);
+%     fextra_sampled = fextra_tr(idx_samples);
+%     fc_sampled = fc_tr(idx_samples);
+    
+    
     y = energy_sampled;
-    X = [rsoma_sampled fsoma_sampled fsup_sampled];
+    X = [rsoma_sampled fsoma_sampled fsup_sampled];%De_tr fneurite_tr fextra_tr fc_tr
 
-
+    
     size_x=size(X);
     ncomp=size_x(2);%ncomp<=nvariables
 
@@ -46,7 +101,7 @@ for i=1:10000
         beta_fsoma(i)=NaN;
         beta_fsup(i)=NaN;
     else
-
+    %new_ncomp=2;
     [XL,yl,XS,YS,beta,PCTVAR,MSE,stats] = plsregress(X,y,new_ncomp);
 
     beta_rsoma(i)=beta(2);
@@ -75,6 +130,9 @@ xlabel('fsup beta coefficients','FontWeight','bold','FontSize',12);
 ylabel('Counts','FontWeight','bold','FontSize',12);
 title(strcat(energy,'vs Soma microparameters'));
 grid on
+
+
+
 
 %For the central limit theorem, the mean has to converge to zero
 mean_beta_fsoma=mean(beta_fsoma);
@@ -118,6 +176,7 @@ std_beta_fsoma=std(beta_fsoma);
 std_beta_rsoma=std(beta_rsoma);
 std_beta_fsup=std(beta_fsup);
 
+%%
 figure,
 plot(0:ncomp,PLSmsep(2,:),'b-o');
 xlabel('Number of components','FontWeight','bold');
